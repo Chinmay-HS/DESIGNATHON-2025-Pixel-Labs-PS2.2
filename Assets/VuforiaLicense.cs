ba474de797705fd782d5203a541ea3aefe70be03
omawadhoot <omawadhoot@outlook.com>
2025-03-02 06:35:48 +0530

    V1 of Cost Estimation Module Implemented with Few Bugs of Customization Module to be fixed yet

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QWQxTEt3ai8vLy8vQUFBQm1hbm03czNMQ0U2YnJ4ZnpyT29Xa3ZtRGRMdVJQa1dta2dhMnovTGFidVNvRTh2SFBqZk9MNHRKY0M1WW5jMnlvRGZlSzNQR0lUOUMvV1luOTEwQnJrTE9LUmE3aDI3MFdIVVNHQkJaU0prYU1tZzFIcEQvcnZWSEhsamhxbjdNcEZZQ2RLQlJoM1BNRmVDNVFpVW1wTHR5VmVweUVTUHpBM3o0cGg5Z0hYcUhwd2k5RURiRktEZHVhWkt3WjhiaEhSb0YrVWxQSjlPbkUxR2RLN3ROQ2FxQnVVVmpYVGlpb0JOZFlnR1hGVWNUUkltY0RiSHJRZ0dRd2M1NmJxejY1ZENCQnk2QUJ3UnY4MDhRWGpudnhoSWUydUJYRE16VDFKZnJ1MUJialA4bHlpVkhEdVVBcFptWktjcWY0MGdxMnFQMjdIM3NKSUdYUTFpY09ROTJoUXJlVlBCRFhJdTZKc24wZFZsbnd2V2M=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
